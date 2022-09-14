9ef0af590575a78718e8e43d394a6e54aab49789
JeferProgramer <jefer.talento.11@gmail.com>
2022-09-14 09:17:57 -0500

    use listcontext provider for the render if is mobile or not , render bullets group or child

--- react/components/BulletGroup/index.tsx
+++ react/components/BulletGroup/index.tsx
@@ -1,8 +1,40 @@
 import React, {PropsWithChildren}from "react"
+import {useListContext, ListContextProvider} from "vtex.list-context"
+import { BulletsSchema } from "./BulletTypes"
+import {useDevice} from "vtex.device-detector"
+import { getBulletsAsTSXlist } from "./modules/bulletsAsList"
 
-const BulletGroup = () => {
+export interface BulletGroupProps{
+  bullets: BulletsSchema
+}
+
+const BulletGroup = ({
+  bullets,
+  children
+}: PropsWithChildren<BulletGroupProps>) => {
+  const {isMobile} = useDevice();
+  const {list} = useListContext() || []
+  console.log("estos son los bullets  " , bullets)
+  console.log(list)
+
+  const bulletsGroup = getBulletsAsTSXlist(bullets)
+  const newListContextValue = list.concat(bulletsGroup)
+
+  if (false){
+    children
+  }
   return(
-    <div>Aqui tendremos un list context</div>
+    <ListContextProvider list={newListContextValue}>
+      {
+        isMobile
+        ?
+          <div className={}>
+            {bulletsGroup}
+          </div>
+        :
+        children
+      }
+    </ListContextProvider>
   )
 }
 export default BulletGroup
