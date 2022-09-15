b4216c136e6e2f526fadb97410022267cb19edab
JeferProgramer <jefer.talento.11@gmail.com>
2022-09-15 11:33:55 -0500

    feat: i create the types and the interface for the component bullet for typescript

---
+++ react/components/BulletGroup/BulletTypes.ts
@@ -0,0 +1,12 @@
+export type BulletsSchema = Array<{
+  image: string
+  titleBullet: string,
+  link?: LinkProps
+}>
+export interface LinkProps{
+  url: string
+  attributeNoFollow?: boolean
+  attributeTitle?: string
+  openNewTab?: boolean
+  newTab?: boolean
+}
